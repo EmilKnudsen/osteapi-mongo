802aa9e4f9daecb0d20a8413be81ec10f4459564
Emil <Emilknudsen1@live.dk>
2020-12-15 10:24:45 +0100

    dynamic result

--- cheeses.route.js
+++ cheeses.route.js
@@ -26,7 +26,16 @@ module.exports = function(app) {
     app.get("/api/v1/cheeses", async function(request, response, next) {
     try {
         var result = await Cheese.find();
-        response.json(result);
+        
+        var output = {
+            count: result.length,
+            next: `${request.protocol}://${request.hostname}${ request.hostname == "localhost" ? ":" + process.env.PORT : "" }${ request.url }?offset=20`,
+            previous: null,
+            url: `${request.protocol}://${request.hostname}${ request.hostname == "localhost" ? ":" + process.env.PORT : "" }${ request.url }`,
+            results: result
+        }
+        response.json(output);
+
     } catch (error) {
         return next(error);
     }
